d49a0fa5dd3efc09bd828eed85606d1397caff75
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-05-12 14:38:40 -0500

    book constructor added AddobjectToArray proto func added
    script.js updated

--- js/script.js
+++ js/script.js
@@ -9,6 +9,21 @@ function Book(title, author, numpages, editorial, readflag){
 }
 
 Book.prototype.addBookToLibrary = function () {
+  if(typeof(this.readflag) != "boolean"){
+    this.readflag = (document.getElementById('BookRead').checked) ? true : false;
+  }
   bookArray.push(this);
   return this;
 };
+
+function testAdding(){
+  let testbook1 = new Book ("Book title", "book Author", 1000, "Debolsillo", true)
+  testbook1.addBookToLibrary();
+  return bookArray;
+}
+
+let testbtn = document.getElementById('TestButton');
+
+testbtn.onclick = function() {
+  console.log(testAdding());
+}
