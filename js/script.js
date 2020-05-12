269ecc1e5f1ba52d119f9bf46e6f6086c77da4ed
firmo holanda <firmo@viize.com>
2020-05-12 18:43:01 -0300

    add saveLocalAndRender function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+let index = 0;
 let myLibrary = [];
 
 function Book(title, author, numpages, editorial, readflag){
@@ -21,8 +22,8 @@ function addBookToLibrary(){
   });
   let newBook = new Book(addTitle, addAuthor, addPages, addEditorial, addStatus);
   myLibrary.push(newBook);
-  console.log(addStatus);
-  console.log(myLibrary);
+  //console.log(addStatus);
+  //console.log(myLibrary);
   render();
 }
 
@@ -30,11 +31,12 @@ function addBookToLibrary(){
 // var x = row.insertCell(0);
 // x.innerHTML = "New cell";
 
-  function render(){
-    const table = document.getElementById('lib-table');
-    table.innerHTML = "";
-    let index = 1;
-    myLibrary.forEach((book) => {
+function render(books){
+  const table = document.getElementById('lib-table');
+  table.innerHTML = "";
+  let index = 1;
+
+  while (index < books.length) {
       let tableRow = table.insertRow();
       let indexCol = document.createElement('th');
       indexCol.innerHTML = index;
@@ -42,42 +44,58 @@ function addBookToLibrary(){
       tableRow.appendChild(indexCol);
 
       let titleCol = tableRow.insertCell(1);
-      titleCol.innerHTML = book.title;
+      titleCol.innerHTML = books[index].title;
 
       let authorCol = tableRow.insertCell(2);
-      authorCol.innerHTML = book.author;
+      authorCol.innerHTML = books[index].author;
 
       let pagesCol = tableRow.insertCell(3);
-      pagesCol.innerHTML = book.numpages;
+      pagesCol.innerHTML = books[index].numpages;
 
       let editorCol = tableRow.insertCell(4);
-      editorCol.innerHTML = book.editorial;
+      editorCol.innerHTML = books[index].editorial;
 
+      // status btn
       let readCol = tableRow.insertCell(5);
       let readButton = document.createElement('button');
-      readButton.innerText = (book.readflag) ? "Read" : "Reading";
-      readButton.classList.add("btn", "btn-outline-info");
-      readButton.onclick = function(){
+      readButton.innerText = (book.readflag) ? "read" : "reading";
+      //readButton.classList.add("btn", "btn-outline-info");
+      readButton.setAttribute("class", "btn btn-outline-info");
+
+      readButton.addEventListener("click", function () {
         book.readflag = !book.readflag;
-        readButton.classList.remove("btn-outline-info");
-        readButton.classList.add("btn-outline-success");
-      };
+        readButton.setAttribute("class", "btn-outline-success");
+      });
+
+      //readButton.onclick = function(){
+        //book.readflag = !book.readflag;
+        //readButton.classList.remove("btn-outline-info");
+        //readButton.classList.add("btn-outline-success");
+        //readButton.setAttribute("class", "btn-outline-success");
+      //};
+
       readCol.appendChild(readButton);
+      console.log(myLibrary);
 
-      let removeCol = tableRow.insertCell(6);
-      let removeButton = document.createElement('button');
-      removeButton.innerText = "remove";
-      removeButton.classList.add("btn", "btn-danger");
-      readButton.onclick = function(){
-        myLibrary.splice(indexOf(book), 1);
-      };
-      removeCol.appendChild(removeButton);
-
-    });
-
+      // remove btn
+      //let removeCol = tableRow.insertCell(6);
+      //let removeButton = document.createElement('button');
+      //removeButton.innerText = "remove";
+      //removeButton.classList.add("btn", "btn-danger");
+      //removeButton.onclick = function(){
+      //  myLibrary.splice(myLibrary.indexOf(book), 1);
+      //};
+      //removeCol.appendChild(removeButton);
   }
 //function testAdding(){
 //  let testbook1 = new Book ("Book title", "book Author", 1000, "Debolsillo", true)
 //  testbook1.addBookToLibrary();
 //  return bookArray;
 //}
+}
+
+// save to local storage and render lib
+function saveLocalAndRender() {
+  localStorage.setItem("myLib", JSON.stringify(myLibrary));
+  render(myLibrary);
+}
